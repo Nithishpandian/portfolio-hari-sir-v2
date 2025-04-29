b3bf831715ef619dce7d5a2c5a6c4507c05da987
Nithishpandian <nithishpandiyan75@gmail.com>
2025-04-29 19:51:12 +0530

    added certificate logos

--- components/certifications/Certifications.tsx
+++ components/certifications/Certifications.tsx
@@ -4,6 +4,9 @@ import certificate2 from "@/assets/images/certificate-2.png";
 import certificate3 from "@/assets/images/certificate-3.png";
 import certificate4 from "@/assets/images/certificate-4.png";
 import Image from "next/image";
+import ec_council_logo from "@/assets/images/ec-council-logo.webp";
+import cisco_logo from "@/assets/images/C-CISO-logo.svg";
+import kba_logo from "@/assets/images/kba_logo.png";
 import Marquee from "react-fast-marquee";
 
 const Certifications = () => {
@@ -13,14 +16,41 @@ const Certifications = () => {
       <div className="absolute top-20 left-0 w-[120%] -ml-[10%] z-30">
         <div className="w-full bg-[#acd800] py-4 transform -rotate-2 overflow-hidden opacity-80">
           <div className="text-[#fafaf8] text-xl md:text-2xl font-bold px-4 whitespace-nowrap">
-            <Marquee speed={80} gradient={false}>
-              <span className="mx-3">EC-COUNCIL CERTIFIED ETHICAL HACKER</span>
+            <Marquee pauseOnHover speed={80} gradient={false}>
+              <span className="mx-3 flex items-center gap-1">
+                <Image
+                  className=" w-24 h-6 object-contain"
+                  src={ec_council_logo}
+                  alt="EC-Council"
+                />
+                EC-COUNCIL
+                <span>CERTIFIED ETHICAL HACKER</span>
+              </span>
               <span className="mx-3">♦</span>
-              <span className="mx-3">CERTIFIED EC-COUNCIL INSTRUCTOR</span>
+              <span className="mx-3 flex items-center gap-1">
+                <Image
+                  className=" w-24 h-6 object-contain"
+                  src={ec_council_logo}
+                  alt="EC-Council"
+                />
+                CERTIFIED EC-COUNCIL INSTRUCTOR
+              </span>
               <span className="mx-3">♦</span>
-              <span className="mx-3">KBA CERTIFIED BLOCKCHAIN ASSOCIATE</span>
+              <span className="mx-3 flex items-center gap-4">
+                <Image
+                  className=" w-fit h-7 object-contain"
+                  src={kba_logo}
+                  alt="CISCO"
+                />
+                KBA CERTIFIED BLOCKCHAIN ASSOCIATE
+              </span>
               <span className="mx-3">♦</span>
-              <span className="mx-3">
+              <span className="mx-3 flex items-center gap-1">
+                <Image
+                  className=" w-fit -mr-5 h-8 object-cover"
+                  src={cisco_logo}
+                  alt="CISCO"
+                />
                 EC-COUNCIL CERTIFIED CHIEF INFORMATION SECURITY OFFICER
               </span>
               <span className="mx-3">♦</span>
@@ -30,8 +60,7 @@ const Certifications = () => {
       </div>
 
       <div className="absolute top-20 left-0 w-[120%] -ml-[10%]">
-        <div className="w-full bg-[#1d1d1d] py-8 transform rotate-4 overflow-hidden ">
-        </div>
+        <div className="w-full bg-[#1d1d1d] py-8 transform rotate-4 overflow-hidden "></div>
       </div>
 
       {/* Certificate marquee */}
